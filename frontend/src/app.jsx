dd03b75fefd4e1bb2ee4715a4042f678f1e97ee6
Apostolos Tsano <tolitsano18@gmail.com>
2026-06-17 16:53:01 -0400

    changed url

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -6,14 +6,14 @@ export default function App(){
             <header className='navbar'>
                 <h1 className = 'title'>SwingSight</h1>
                 <nav className = 'navButtons'>
-                    <NavLink className = 'link' to="/">Signals</NavLink>
+                    <NavLink className = 'link' to="/signals">Signals</NavLink>
                     <NavLink className = 'link' to = "/backtesting">BackTest</NavLink>
                 </nav>
             </header>
 
             <main>
                 <Routes>
-                    <Route path = "/" element = {<h2>Live Analysis Page</h2>} />
+                    <Route path = "/signals" element = {<h2>Live Analysis Page</h2>} />
                     <Route path = "/backtesting" element = {<h2>Back Testing Page</h2>} />
                 </Routes>
             </main>
