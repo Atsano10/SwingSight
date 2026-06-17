504e72d990691abd37a09e9fe08ad0e93c460e5f
Apostolos Tsano <tolitsano18@gmail.com>
2026-06-17 02:54:46 -0400

    fixing nav bar --still in work

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,1 +1,22 @@
+import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom'
 
+export default function App(){
+    return(
+        <BrowserRouter>
+            <header className='navbar'>
+                <h1 className = 'title'>SwingSight</h1>
+                <nav className = 'navButtons'>
+                    <NavLink className = 'link' to="/">Signals</NavLink>
+                    <NavLink className = 'link' to = "/backtesting">BackTest</NavLink>
+                </nav>
+            </header>
+
+            <main>
+                <Routes>
+                    <Route path = "/" element = {<h2>Live Analysis Page</h2>} />
+                    <Route path = "/backtesting" element = {<h2>Back Testing Page</h2>} />
+                </Routes>
+            </main>
+        </BrowserRouter>
+    )
+}
