81a1fe0fd2b813de4be06876ff7adeeee5e55d27
Apostolos Tsano <tolitsano18@gmail.com>
2026-06-18 01:38:41 -0400

    fixed table layout to look cleaner

--- frontend/src/components/tradeTable.jsx
+++ frontend/src/components/tradeTable.jsx
@@ -1,40 +1,104 @@
 export default function Table(){
     return(
         <>
-            <div className = 'signal'>
-                <div>Ticker:</div>
-                <div>Entry:</div>
-                <div>Stop:</div>
-                <div>Target:</div>
-                <div>Shares:</div>
+            <div className='signal'>
+                <div className='signalTicker'>NOK</div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Entry</span>
+                    <span className='signalValue'>13.83</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Stop</span>
+                    <span className='signalValue'>13.18</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Target</span>
+                    <span className='signalValue'>15.13</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Shares</span>
+                    <span className='signalValue'>153.85</span>
+                </div>
             </div>
-            <div className = 'signal'>
-                <div>Ticker:</div>
-                <div>Entry:</div>
-                <div>Stop:</div>
-                <div>Target:</div>
-                <div>Shares:</div>
+
+            <div className='signal'>
+                <div className='signalTicker'>NOK</div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Entry</span>
+                    <span className='signalValue'>13.83</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Stop</span>
+                    <span className='signalValue'>13.18</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Target</span>
+                    <span className='signalValue'>15.13</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Shares</span>
+                    <span className='signalValue'>153.85</span>
+                </div>
             </div>
-            <div className = 'signal'>
-                <div>Ticker:</div>
-                <div>Entry:</div>
-                <div>Stop:</div>
-                <div>Target:</div>
-                <div>Shares:</div>
+
+            <div className='signal'>
+                <div className='signalTicker'>NOK</div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Entry</span>
+                    <span className='signalValue'>13.83</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Stop</span>
+                    <span className='signalValue'>13.18</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Target</span>
+                    <span className='signalValue'>15.13</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Shares</span>
+                    <span className='signalValue'>153.85</span>
+                </div>
             </div>
-            <div className = 'signal'>
-                <div>Ticker:</div>
-                <div>Entry:</div>
-                <div>Stop:</div>
-                <div>Target:</div>
-                <div>Shares:</div>
+
+            <div className='signal'>
+                <div className='signalTicker'>NOK</div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Entry</span>
+                    <span className='signalValue'>13.83</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Stop</span>
+                    <span className='signalValue'>13.18</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Target</span>
+                    <span className='signalValue'>15.13</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Shares</span>
+                    <span className='signalValue'>153.85</span>
+                </div>
             </div>
-            <div className = 'signal'>
-                <div>Ticker:</div>
-                <div>Entry:</div>
-                <div>Stop:</div>
-                <div>Target:</div>
-                <div>Shares:</div>
+            
+            <div className='signal'>
+                <div className='signalTicker'>NOK</div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Entry</span>
+                    <span className='signalValue'>13.83</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Stop</span>
+                    <span className='signalValue'>13.18</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Target</span>
+                    <span className='signalValue'>15.13</span>
+                </div>
+                <div className='signalField'>
+                    <span className='signalLabel'>Shares</span>
+                    <span className='signalValue'>153.85</span>
+                </div>
             </div>
         </>
     )
