24c486da974ec8067e06d9ab345a1bc022709061
Apostolos Tsano <tolitsano18@gmail.com>
2026-06-19 03:39:14 -0400

    added sliding options for logout and routing on smaller screens

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,9 +1,10 @@
+import { useState } from 'react'
 import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom'
 import BackTesting from './pages/backTesting.jsx'
 import LiveAnalysis from './pages/liveAnalysis.jsx'
-import Home from './pages/home.jsx'
 
 export default function App(){
+    const [isOpen, setIsOpen] = useState(false)
     return(
         <BrowserRouter>
             <header className='navbar'>
@@ -11,12 +12,20 @@ export default function App(){
                 <nav className = 'navButtons'>
                     <NavLink className = 'link' to="/signals">Signals</NavLink>
                     <NavLink className = 'link' to = "/backtesting">BackTest</NavLink>
+                    <button className = 'navOptions' onClick={() => setIsOpen(!isOpen)}>&#9776;</button>
                 </nav>
             </header>
-
+            <div className={`sidePanel ${isOpen ? 'open' : ''}`}>
+                <div className = "sideTop">
+                    <div className = 'sideTitle'>SwingSight</div>
+                    <button className = 'sideClose' onClick = {() => setIsOpen(false)} >X</button>
+                </div>
+                <NavLink className = 'sideLink' to="/signals" onClick = {() => setIsOpen(false)}>Signals</NavLink>
+                <NavLink className = 'sideLink' to = "/backtesting" onClick = {() => setIsOpen(false)}>BackTest</NavLink>
+                <button className = 'logOut'>Log Out</button>
+            </div>
             <main>
                 <Routes>
-                    <Route path="/" element={<Home/>} />
                     <Route path = "/signals" element = {<LiveAnalysis />} />
                     <Route path = "/backtesting" element = {<BackTesting />} />
                 </Routes>
