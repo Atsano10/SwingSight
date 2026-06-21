2975a4364f42511fee4e79e559f7e6fe246a33bd
Apostolos Tsano <tolitsano18@gmail.com>
2026-06-21 01:50:53 -0400

    added supabase connection

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -1,3 +1,6 @@
-// Initialize and export the Supabase client here using createClient from '@supabase/supabase-js'
-// Pass in your VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY from import.meta.env
-// This exported client is what every other file uses to talk to Supabase
+import {createClient} from '@supabase/supabase-js'
+
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey =  import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseURL, supabaseAnonKey)
